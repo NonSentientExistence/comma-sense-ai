1571d0077b2ac13cd09597a8f14026e37931bb73
NonSentientExistence <david@venngren.se>
2026-05-30 21:00:31 +0200

    Completed CSV upload. Added data/stats for describe return

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
 from fastapi.responses import HTMLResponse
+import io
+import pandas as pd
+import numpy as np
 
 from typing import Annotated
+
+from app import data
 
 app = FastAPI()
 
@@ -14,7 +19,7 @@ async def root():
 async def main():
     content = """
 <body>
-<form action="/files/" enctype="multipart/form-data" method="post">
+<form action="/data/upload/" enctype="multipart/form-data" method="post">
 <label for="file-upload" class="form-label">Select file:</label>
 <input
     type="file"
@@ -22,18 +27,39 @@ async def main():
     name="file"
     accept=".csv"
     aria-describedby="file-help"
-  />
+/>
 <div id="file-help" class="form-text">Accepted formats: CSV</div>
+<select name="separator" id="separator">
+    <option value=",">Comma (,)</option>
+    <option value=";">Semicolon (;)</option>
+    <option value="\t">Tab</option>
+</select>
 <button type="submit">Upload File</button>
 </form>
+
 </body>
     """
     return HTMLResponse(content=content)
 
 
 @app.post("/data/upload/")
-async def upload_file(file: UploadFile):
-   if not file.file.filename.endswith(".csv"):
-       raise HTTPException(status_code=400, detail="Only CSV files allowed")
-   
-   contents = await file.read()
+async def upload_file(
+    file: UploadFile,
+    separator: str = Form(default=",")
+):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Only CSV files allowed")
+
+    contents = await file.read()
+    data.current_df = pd.read_csv(io.BytesIO(contents), sep = separator)
+
+    return {
+        "rows": len(data.current_df),
+        "columns": data.current_df.columns.tolist()
+    }
+
+@app.get("/data/stats")
+async def data_stats():
+    if data.current_df is None:
+        raise HTTPException(status_code=404, detail="No CSV data has been uploaded")
+    return data.current_df.describe().replace({np.nan: None}).to_dict()
